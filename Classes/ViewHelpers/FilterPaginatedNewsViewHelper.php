02a89129e526eb40a999c20d07ee6cffd9a975b2
Raphael Zschorsch <zschorsch@medialis.net>
2013-10-28 20:10:58 +0100

    New ViewHelper
    news Extension: Filter paginated news for correct languages

---
+++ Classes/ViewHelpers/FilterPaginatedNewsViewHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+class Tx_Meddevutils_ViewHelpers_FilterPaginatedNewsViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractConditionViewHelper {
+
+    /**
+     * Render everything
+     *
+     * @param Tx_Extbase_Persistence_QueryResultInterface $objects
+     * @param string $language
+     * @return string
+     */
+    public function render(Tx_Extbase_Persistence_QueryResultInterface $objects, $language = 0) {
+        // Get query
+        $query = $objects->getQuery();
+        //$query->statement('SELECT * FROM `tx_news_domain_model_news` WHERE hidden=0 AND deleted=0 AND sys_language_uid='.$language);
+
+        // Get all items
+        $query->getQuerySettings()->setRespectEnableFields(FALSE);
+
+        // Limit items to language
+        $constraints[] = $query->like('sys_language_uid',$language);
+
+        // Remove wrong language items
+        if(count($constraints)>0) {
+            $query->matching($query->logicalAnd($constraints));
+        }
+
+        // Execute query
+        $modifiedObjects = $query->execute();
+
+        //Tx_Extbase_Utility_Debugger::var_dump($modifiedObjects);
+
+        return $modifiedObjects;
+    }
+
+}
+
+?>
